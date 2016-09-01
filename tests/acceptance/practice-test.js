23f0c7de6384d1ad60e7fae6e66576b806e44ed5
Yohan Mishkin <yohanmishkin@protonmail.com>
2016-09-01 01:04:26 -0400

    Added navigation back to the home page from /practice.
    - Added flash card images

--- tests/acceptance/practice-test.js
+++ tests/acceptance/practice-test.js
@@ -1,4 +1,4 @@
-import { test } from 'qunit';
+import { test, skip } from 'qunit';
 import moduleForAcceptance from 'unitflash/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | practice');
@@ -13,3 +13,22 @@ test('Can navigate to /practice', function(assert) {
   });
 });
 
+test('Can navigate back to home page from /practice', function(assert) {
+	visit('/practice');
+
+	click('.test-nav-home');
+
+	andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+
+skip('/practice shows random card', function(assert) {
+	// generate first random card
+
+	visit('/practice');
+
+	andThen(function() {
+		assert.equal(find('.test-card-practice').text(), 'PRACTICE');
+	});
+});
