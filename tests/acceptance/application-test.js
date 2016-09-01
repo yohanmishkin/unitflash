73c40ca665bb1461eca118814edc38e354510eee
Yohan Mishkin <yohanmishkin@yahoo.com>
2016-09-01 00:18:20 -0400

    Layout of home page and some basic styling

---
+++ tests/acceptance/application-test.js
@@ -0,0 +1,22 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'unitflash/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | application');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+
+
+test('Home page has navigation', function(assert) {
+	visit('/');
+
+	andThen(function() {
+		assert.equal(find('.test-header-practice').text(), 'PRACTICE');
+		assert.equal(find('.test-header-play').text(), 'PLAY');
+	});
+});
