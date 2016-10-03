05c31183e3369d39c74aac22960f9f39c82585d4
Tommy Jebb <tjebb@steadfast.com>
2016-10-03 14:02:29 -0400

    Moved jpgs to /public/cards

--- tests/acceptance/practice-test.js
+++ tests/acceptance/practice-test.js
@@ -27,19 +27,19 @@ test('Clicking flips card', function(assert) {
 	visit('/practice/1');
 
 	andThen(function() {
-		assert.equal(find('.test-unitflash-card-image').attr('src'), '/assets/images/1f.jpg', 'Showing the front image');
+		assert.equal(find('.test-unitflash-card-image').attr('src'), '/cards/1f.jpg', 'Showing the front image');
 	});
 
 	click('.test-unitflash-card');
 
 	andThen(function() {
-		assert.equal(find('.test-unitflash-card-image').attr('src'), '/assets/images/1b.jpg', 'Showing the back image');
+		assert.equal(find('.test-unitflash-card-image').attr('src'), '/cards/1b.jpg', 'Showing the back image');
 	});
 
 	click('.test-unitflash-card');
 
 	andThen(function() {
-		assert.equal(find('.test-unitflash-card-image').attr('src'), '/assets/images/1f.jpg', 'Showing the front image again');
+		assert.equal(find('.test-unitflash-card-image').attr('src'), '/cards/1f.jpg', 'Showing the front image again');
 	});	
 });
 
@@ -56,7 +56,7 @@ test('Can go to next card', function(assert) {
 	click('.test-next-card');
 
 	andThen(function() {
-		assert.equal(find('.test-unitflash-card-image').attr('src'), '/assets/images/3f.jpg', 'Showing the front image');
+		assert.equal(find('.test-unitflash-card-image').attr('src'), '/cards/3f.jpg', 'Showing the front image');
 	});
 });
 
