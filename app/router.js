c887565f49988144b5f7d1669589a0b9436dbf08
Yohan Mishkin <yohanmishkin@yahoo.com>
2016-10-06 02:01:24 -0400

    Starting up Firebase again

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('index', { path: '/' });
   this.route('practice', { path: '/practice/:card_id'});
+  this.route('upload');
 });
 
 export default Router;
